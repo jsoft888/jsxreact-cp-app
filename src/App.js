719cb45128b645464ebd2cbd2bcfade2131fd1af
MAC <mac@MACs-MacBook-Pro.local>
2023-11-03 21:26:22 -0700

    adding jsxreact-cp-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import product from "./Product";
+import Name from "./Name";
+import Price from "./Price";
+import Description from "./Description";
+import Image from "./Image";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Container } from "react-bootstrap";
+
+const firstName = "John";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Card style={{ width: "18rem" }}>
+        <Image imageUrl={product.imageUrl} />
+        <Card.Body>
+          <Name name={product.name} />
+          <Price price={product.price} />
+          <Description description={product.description} />
+        </Card.Body>
+      </Card>
+      <p>Hello, {firstName ? firstName : "there"}!</p>
+      {firstName && <Image imageUrl={product.imageUrl} />}
+    </Container>
   );
 }
 
---
+++ src/Product.js
@@ -0,0 +1,9 @@
+const product = {
+    name: "babaijebu",
+    price: 100,
+    description: "You play we pay.",
+    imageUrl: "https://blog.babaijebu.ng/wp-content/uploads/2015/08/Baba-Ijebu-Lotto-Goes-Online-886x550.png",
+  };
+  
+  export default product;
+  
